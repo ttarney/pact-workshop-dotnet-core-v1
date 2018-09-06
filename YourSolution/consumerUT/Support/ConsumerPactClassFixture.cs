158aa67fe571c2177204a80f1715bfd2d0c76d51
Theodore Tarney <theodore.tarney@pioneer.com>
2018-09-06 14:00:18 -0500

    update

---
+++ YourSolution/consumerUT/Support/ConsumerPactClassFixture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using PactNet;
+using PactNet.Mocks.MockHttpService;
+
+namespace consumerUT.Support
+{
+    public class ConsumerPactClassFixture : IDisposable
+    {
+        public IPactBuilder PactBuilder { get; private set; }
+        public IMockProviderService MockProviderService { get; private set; }
+
+        public int MockServerPort { get { return 9222; } }
+
+        private bool _disposedValue = false;
+        public string MOckProviderServiceBaseUri
+        {
+            get
+            {
+                return string.Format("http://localhost:{0}", MockServerPort);
+            }
+        }
+
+        public ConsumerPactClassFixture()
+        {
+            PactConfig pactConfig = new PactConfig()
+            {
+                SpecificationVersion = "2.0.0",
+                PactDir = @"..\..\..\..\..\pacts",
+                LogDir = @".\pact_logs"
+            };
+
+            PactBuilder = new PactBuilder(pactConfig);
+
+            PactBuilder.ServiceConsumer("Consumer")
+                .HasPactWith("Provider");
+
+            MockProviderService = PactBuilder.MockService(MockServerPort);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // This will save the pact file once finished.
+                    PactBuilder.Build();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+    }
+}
---
+++ YourSolution/consumerUT/UnitTest1.cs
@@ -0,0 +1,53 @@
+using consumerUT.Support;
+using PactNet.Mocks.MockHttpService;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace consumerUT
+{
+    public class ConsumerPactTests : IClassFixture<ConsumerPactClassFixture>
+    {
+        private IMockProviderService _mockProviderService;
+        private string _mockProviderServiceBaseUri;
+
+        public ConsumerPactTests(ConsumerPactClassFixture fixture)
+        {
+            _mockProviderService = fixture.MockProviderService;
+            _mockProviderService.ClearInteractions();
+            _mockProviderServiceBaseUri = fixture.MOckProviderServiceBaseUri;
+        }
+
+        [Fact]
+        public void provider_should_handle_valid_date_parameter()
+        {
+            string invalidRequestMessage = "validDateTime is not a date or time";
+            _mockProviderService.Given("There is data")
+                .UponReceiving("An invalid GET request for Date Validation with invalid date parameter")
+                .With
+                    (
+                        new PactNet.Mocks.MockHttpService.Models.ProviderServiceRequest
+                        {
+                            Method = PactNet.Mocks.MockHttpService.Models.HttpVerb.Get,
+                            Path = "/api/provider",
+                            Query = "validDateTime=lolz"
+                        }
+                    )
+                    .WillRespondWith
+                    (
+                        new PactNet.Mocks.MockHttpService.Models.ProviderServiceResponse
+                        {
+                            Status = 400,
+                            Headers = new Dictionary<string, object>
+                            {
+                                { "Content-TYpe", "application/json; charset=utf-8" }
+                            },
+                            Body = new
+                            {
+                                message = invalidRequestMessage
+                            }
+                        });
+        }
+    }
+
+}
